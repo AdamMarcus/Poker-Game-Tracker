82dd21149ce3e62fc2aed08375626533effd757f
AdamMarcus <adam.marcus.austin@gmail.com>
2017-07-16 10:36:30 -0600

    Added a window to prompt descisions from the user.

---
+++ src/com/company/DescisionPropmterAlert.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Created by adamaustin on 7/14/17.
+ */
+public class DescisionPropmterAlert {
+    static boolean result;
+
+    final static int ALERT_BOX_WIDTH = 200;
+    final static int ALERT_BOX_HEIGHT = 100;
+    final static double EDGE_OFFSET = 10.0;
+
+    public static boolean displayAlert(String title, String message) {
+        Stage window = new Stage();
+        window.setMinWidth(ALERT_BOX_WIDTH);
+        window.setMinHeight(ALERT_BOX_HEIGHT);
+        window.setTitle(title);
+        window.initModality(Modality.APPLICATION_MODAL);
+
+        Label alertText = new Label(message);
+        alertText.setAlignment(Pos.CENTER);
+
+        Button noButton = new Button("No");
+        noButton.setOnAction(e -> {
+            result = false;
+            window.close();
+        });
+        noButton.setAlignment(Pos.BOTTOM_RIGHT);
+
+        Button yesButton = new Button("Yes");
+        yesButton.setOnAction(e -> {
+            result = true;
+            window.close();
+        });
+        yesButton.setAlignment(Pos.BOTTOM_RIGHT);
+
+        AnchorPane alertPane = new AnchorPane();
+        alertPane.getChildren().addAll(alertText, noButton, yesButton);
+        alertPane.setBottomAnchor(yesButton, EDGE_OFFSET);
+        alertPane.setRightAnchor(yesButton, EDGE_OFFSET);
+
+        alertPane.setBottomAnchor(noButton, EDGE_OFFSET);
+        alertPane.setLeftAnchor(noButton, EDGE_OFFSET);
+
+        Scene alertScene = new Scene(alertPane);
+        window.setScene(alertScene);
+        window.showAndWait();
+
+        return result;
+    }
+}
